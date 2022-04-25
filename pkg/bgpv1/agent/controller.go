a4331db726007eb93aa772a7f1d383fdc021fb48
Louis DeLosSantos <louis.delos@isovalent.com>
2022-04-25 14:08:44 -0400

    bgpv1,controller: export a method for external callers to signal reconciliation
    this commit exports a method called "Signal" on the BGP controller's
    method set.
    
    this provides a way for external callers to trigger a reconciliation of
    the BGP control plane.
    
    Signed-off-by: Louis DeLosSantos <louis.delos@isovalent.com>

--- pkg/bgpv1/agent/controller.go
+++ pkg/bgpv1/agent/controller.go
@@ -189,6 +189,15 @@ func NewController(ctx context.Context, rtMgr BGPRouterManager, opts ...Controll
 	return &c, nil
 }
 
+// Signal provides a thread-safe way to trigger the BGP Control Plane controller's
+// reconciliation loop.
+//
+// It is guaranteed, bar any fatal errors, that the reconciliation loop is ran
+// sometime after this method returns.
+func (c *Controller) Signal() {
+	c.Sig.Event(struct{}{})
+}
+
 // Run places the Controller into its control loop.
 //
 // Kubernetes shared informers are started just before entering the long running
