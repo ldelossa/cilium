c8f15a81cf7cdf358d2ae53b40a981800f444aab
Paul Chaignon <paul@cilium.io>
2023-03-14 14:32:33 +0100

    test/srv6: Fixup SID map manually
    We need to manually teach k8s1's datapath about the expected SID for
    End.DT{4,6} on ingress. This is because, when running SRv6 without
    the BGP integration, we don't populate the SID map. It needs a proper
    fix longer term (or we drop CRDs) but for now let's just fixup the
    test since the BGP integration is what users are expected to run.
    
    Note we can hardcode k8s1 here because we know the client pod always
    runs on k8s1 (see manifest.). The VRF ID is hardcoded as well, for
    the same reason.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- test/k8s/srv6.go
+++ test/k8s/srv6.go
@@ -6,6 +6,7 @@ package k8sTest
 import (
 	"context"
 	"fmt"
+	"net"
 
 	. "github.com/onsi/gomega"
 
@@ -71,6 +72,27 @@ var _ = SkipDescribeIf(helpers.DoesNotExistNodeWithoutCilium, "K8sSRv6", func() 
 		Expect(res).Should(helpers.CMDSuccess(), "Error adding pod CIDR IP route for %s", k8s2NodeName)
 	}
 
+	plumbEndDTRule := func(nodeName string) {
+		privateIface, err := k.GetPrivateIface(nodeName)
+		Expect(err).Should(BeNil(), "Cannot retrieve iface of node %s", nodeName)
+
+		nodeIPv6, err := helpers.GetIPv6AddrForIface(k, nodeName, privateIface)
+		Expect(err).Should(BeNil(), "Cannot retrieve IPv6 of node %s", nodeName)
+
+		ip := []byte(net.ParseIP(nodeIPv6))
+		cmd := "bpftool map update pinned /sys/fs/bpf/tc/globals/cilium_srv6_sid key"
+		for _, b := range ip {
+			cmd = fmt.Sprintf("%s 0x%x", cmd, b)
+		}
+		cmd = fmt.Sprintf("%s value 0x0c 00 00 00", cmd)
+		fmt.Println(cmd)
+
+		ciliumPod, err := k.GetCiliumPodOnNode(helpers.K8s1)
+		Expect(err).Should(BeNil(), "Cannot determine cilium pod name")
+		res := k.CiliumExecContext(context.TODO(), ciliumPod, cmd)
+		res.ExpectSuccess("Failed to run command %s", cmd)
+	}
+
 	It("SRv6 encapsulation", func() {
 		srv6YAML := helpers.ManifestGet(k.BasePath(), "srv6.yaml")
 		k.Create(srv6YAML).ExpectSuccess("Unable to create resource %q", srv6YAML)
@@ -89,6 +111,16 @@ var _ = SkipDescribeIf(helpers.DoesNotExistNodeWithoutCilium, "K8sSRv6", func() 
 		}
 
 		installK8s3Routes()
+
+		// We need to manually teach k8s1's datapath about the expected SID for
+		// End.DT{4,6} on ingress. This is because, when running SRv6 without
+		// the BGP integration, we don't populate the SID map. It needs a proper
+		// fix longer term (or we drop CRDs) but for now let's just fixup the
+		// test since the BGP integration is what users are expected to run.
+		// Note we can hardcode k8s1 here because we know the client pod always
+		// runs on k8s1 (see manifest.). The VRF ID is hardcoded as well, for
+		// the same reason.
+		plumbEndDTRule(k8s1NodeName)
 
 		testPodsIPs, err := k.GetPodsIPs(helpers.DefaultNamespace, testPodFilter)
 		Expect(err).Should(BeNil(), "Cannot retrieve pod IPs for %s", testPodFilter)
