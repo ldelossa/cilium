6cf815527cdc8a85757e7f6252798b4c069ef082
Fabio Falzoi <fabio.falzoi@isovalent.com>
2023-03-25 19:34:38 +0100

    operator/dnsclient: Add DNS client cell
    Add a DNS client cell to periodically resolve FQDN groups in the
    background. The new cell supports both TypeA and TypeAAAA queries and
    uses the DNS configuration from /etc/resolv.conf by default.
    
    Since its main use case is for background FQDN group resolution, the
    exported methods trim down the TTL returned by the DNS server taking
    into account the DNS query RTT. The reason to do that is to keep the
    list of IPs in the resulting CIDR group as fresh as possible and thus
    avoiding connectivity disruption due to policies referencing stale IPs.
    
    Also, add a simple unit-style integration test for the DNS client to
    check both TypeA and TypeAAAA queries.
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

---
+++ enterprise/operator/dnsclient/cell.go
@@ -0,0 +1,45 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+package dnsclient
+
+import (
+	"github.com/spf13/pflag"
+
+	"github.com/cilium/cilium/pkg/hive/cell"
+)
+
+const (
+	// ServerAddresses is a list of DNS servers addresses in the form "<ip>:<port>"
+	// to be used from the operator dns client.
+	// If multiple servers are set, the client queries them in the order listed.
+	DNSServerAddresses = "dns-server-addresses"
+)
+
+var Cell = cell.Module(
+	"dns-client",
+	"Isovalent DNS client",
+
+	cell.Config(Config{}),
+	cell.Provide(newClient),
+)
+
+type Config struct {
+	DNSServerAddresses []string
+}
+
+func (def Config) Flags(flags *pflag.FlagSet) {
+	flags.StringSlice(
+		DNSServerAddresses,
+		nil,
+		"A list of DNS server addresses to be used by the operator DNS client for resolution of FQDNs in IsovalentFQDNGroup CRDs. Each address should be in the form \"<ip>:<port>\". "+
+			"When resolving an FQDN, the operator will try to query the first server. If it fails, it will try the next one and so on, following the order specified by the user.",
+	)
+}
