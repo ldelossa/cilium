1ead0d49c557856084d8128a5a364f5b8d71e94e
Fabio Falzoi <fabio.falzoi@isovalent.com>
2023-03-25 19:48:20 +0100

    operator/dnsresolver: Add background FQDN resolver
    The background FQDN resolver wraps a DNS client and performs periodical
    resolution of a given FQDN. The interval between two subsequent queries
    is set to the TTL returned by the DNS server or to the minimum one set
    through an option passed at instantiation time.
    
    The data from the DNS query response are sent to a store to be cached
    until the next resolution.
    
    Co-authored-by: Chris Tarazi <chris@isovalent.com>
    Signed-off-by: Chris Tarazi <chris@isovalent.com>
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

---
+++ enterprise/operator/dnsresolver/test_helpers.go
@@ -0,0 +1,30 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+package dnsresolver
+
+import (
+	"context"
+	"net/netip"
+	"time"
+)
+
+type mockClient struct {
+	ipv4Fn func(ctx context.Context, fqdn string) ([]netip.Addr, []time.Duration, error)
+	ipv6Fn func(ctx context.Context, fqdn string) ([]netip.Addr, []time.Duration, error)
+}
+
+func (c *mockClient) QueryIPv4(ctx context.Context, fqdn string) ([]netip.Addr, []time.Duration, error) {
+	return c.ipv4Fn(ctx, fqdn)
+}
+
+func (c *mockClient) QueryIPv6(ctx context.Context, fqdn string) ([]netip.Addr, []time.Duration, error) {
+	return c.ipv6Fn(ctx, fqdn)
+}
