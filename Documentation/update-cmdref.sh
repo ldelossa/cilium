2064fafe3b958e9ddd24921b09a6475ac78322e1
Fabio Falzoi <fabio.falzoi@isovalent.com>
2023-06-05 21:48:38 +0200

    cmdref: update script with enteprise binaries
    Update cmdref script to rely on enterprise version of both daemon and
    operator.
    
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

--- Documentation/update-cmdref.sh
+++ Documentation/update-cmdref.sh
@@ -10,14 +10,14 @@ cmdref_dir="${script_dir}/cmdref"
 
 generators=(
     "cilium/cilium cmdref -d"
-    "daemon/cilium-agent cmdref"
+    "enterprise/daemon/cilium-agent cmdref"
     "bugtool/cilium-bugtool cmdref -d"
     "cilium-health/cilium-health --cmdref"
-    "operator/cilium-operator --cmdref"
-    "operator/cilium-operator-aws --cmdref"
-    "operator/cilium-operator-azure --cmdref"
-    "operator/cilium-operator-generic --cmdref"
-    "operator/cilium-operator-alibabacloud --cmdref"
+    "enterprise/operator/cilium-operator --cmdref"
+    "enterprise/operator/cilium-operator-aws --cmdref"
+    "enterprise/operator/cilium-operator-azure --cmdref"
+    "enterprise/operator/cilium-operator-generic --cmdref"
+    "enterprise/operator/cilium-operator-alibabacloud --cmdref"
 )
 
 for g in "${generators[@]}" ; do
