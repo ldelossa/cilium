6f24d256eef8e43691fe7ea4fb5994e0fcfa27fd
Fabio Falzoi <fabio.falzoi@isovalent.com>
2023-06-07 16:56:05 +0200

    enterprise: Bootstrap daemon and operator
    Add enterprise daemon and operator as distinct binaries from their OSS
    counterparts.
    
    The enteprise binaries rely on separate cobra.Command(s) and hives to
    extend the OSS features limiting as much as possible the changes to the
    shared part of the codebase.
    
    Also, both enterprise agent and operator relies on a separate go.mod.
    This is done to avoid conflicts when additional packages are imported by
    enterprise-only features.
    
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

---
+++ enterprise/operator/cells.go
@@ -0,0 +1,36 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+package main
+
+import (
+	"github.com/cilium/cilium/operator/cmd"
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/hive/cell"
+)
+
+var (
+	EnterpriseOperator = cell.Module(
+		"enterprise-operator",
+		"Cilium Operator Enterprise",
+
+		cmd.Operator,
+
+		// enteprise-only cells here
+
+		cell.Decorate(
+			func(lc *cmd.LeaderLifecycle) hive.Lifecycle {
+				return lc
+			},
+
+			// enteprise-only cells to be started after leader election here
+		),
+	)
+)
