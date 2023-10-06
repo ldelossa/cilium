19fc21d0f1c72e2a80cdd9cb3347d3fb251e7f29
Jussi Maki <jussi@isovalent.com>
2023-10-06 10:57:43 +0300

    enterprise/daemon: Add cells_test.go to check construction
    Checks that EnterpriseAgent can be populated.
    This validates that:
    
    - All dependencies are satisfied and we catch them in this simple test
      and not when trying to start the agent in a more complicated context.
    
    - No goroutines are forked from provides/invokes.
    
    - No privileged syscalls are made from provides/invokes, which catches at
      least some misbehaving constructors
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

---
+++ enterprise/daemon/cells_test.go
@@ -0,0 +1,44 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.uber.org/goleak"
+
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/metrics"
+)
+
+var goleakOptions = []goleak.Option{
+	// Ignore all the currently running goroutines spawned
+	// by prior tests or by package init() functions (like the
+	// client-go logger).
+	goleak.IgnoreCurrent(),
+	// Ignore goroutines started by the policy trifecta, see [newPolicyTrifecta].
+	goleak.IgnoreTopFunction("github.com/cilium/cilium/pkg/identity/cache.(*identityWatcher).watch.func1"),
+	goleak.IgnoreTopFunction("github.com/cilium/cilium/pkg/trigger.(*Trigger).waiter"),
+}
+
+// TestEnterpriseAgentCell verifies that the EnterpriseAgent can be instantiated with
+// default configuration and thus the EnterpriseAgent hive can be inspected with
+// the hive commands and documentation can be generated from it.
+func TestEnterpriseAgentCell(t *testing.T) {
+	defer goleak.VerifyNone(t, goleakOptions...)
+	defer metrics.Reinitialize()
+
+	logging.SetLogLevelToDebug()
+	err := hive.New(EnterpriseAgent).Populate()
+	require.NoError(t, err, "hive.New(EnterpriseAgent).Populate()")
+}
