a1a5c9c3ced7232df03b1c0d949310a3009c9525
Michi Mutsuzaki <michi@isovalent.com>
2023-11-22 18:46:38 +0000

    Override CILIUM_CLI_VERSION
    With cilium/cilium#29237 and cilium/cilium#29338 merged upstream, we can
    now override and use the enterprise version of cilium-cli for running
    tests. The goal is to backport this to v1.14-ce branch so that we can
    use the enterprise cilium-cli released from the main-ce branch for
    running tests in stable branches starting from v1.14-ce.
    
    Ref: https://isovalent.slack.com/archives/C05188W95GT/p1700144241292359
    
    Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

---
+++ .github/actions/set-env-variables/enterprise-overrides.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# Use this script to override environment variables that get set in the
+# upstream set-env-variables composite action.
+
+set -ex
+
+echo "CILIUM_CLI_RELEASE_REPO=isovalent/cilium-cli-releases" >> "$GITHUB_ENV"
+# renovate: datasource=github-releases depName=isovalent/cilium-cli-releases
+CILIUM_CLI_VERSION="v0.15.13-cee.1"
+echo "CILIUM_CLI_VERSION=$CILIUM_CLI_VERSION" >> "$GITHUB_ENV"
