3f5446ef10418f04779480733fe761190c037c87
Anton Protopopov <aspsk@isovalent.com>
2023-12-07 13:29:00 +0000

    Add make targets for Cilium Mesh and a local smoke-test
    In order to test Cilium Mesh functionality locally, one can run
    kind-based tests.
    
    There are three ways of preparing the cluster and installing cilium
    mesh.  The first one, aka "slow", can be used for an automated
    test environment, as this version builds the required images
    from sources:
    
        make kind-cilium-mesh         # Create two kind clusters
        make kind-cilium-mesh-images  # Build & load images into clusters
        make kind-cilium-mesh-install # Install cilium mesh and interconnect tgws
    
    The second way can be used to deploy a cluster from existing images. Instead of
    the previous steps, use, e.g.,
    
        export DOCKER_DEV_ACCOUNT=aspsk DOCKER_REGISTRY=quay.io LOCAL_IMAGE_TAG=cilium-mesh-lab
        make kind-cilium-mesh              # same: create two kind clusters
        make kind-load-cilium-mesh-images  # instead of building images, load them
        make kind-cilium-mesh-install      # Install cilium mesh and interconnect tgws
    
    Now, if you're a developer actively patching code and wanting to test changes
    fast (and feeling lucky), then do the following.  First, pull some images from
    quay.io (required once):
    
        docker login quay.io
        docker pull quay.io/isovalent-dev/cilium-ci:main
        docker pull quay.io/isovalent-dev/operator-generic-ci:main
        docker pull quay.io/isovalent-dev/clustermesh-apiserver-ci:main
    
    Now the workflow is as follows:
    
        make kind-cilium-mesh               # Create two kind-based k8s clusters
        make kind-cilium-mesh-install-fast  # Install cilium
        make kind-image-enterprise-fast     # Build agent and operator and redeploy fast
    
    After this one can do the following in a loop:
    
        #
        # patch agent/operator/BPF code
        #
        make kind-image-enterprise-fast
    
    Given that one of the two variants was taken, the following command
    can be used to test basic connectivity (it will be replaced by a
    proper `cilium connectivity test` in future):
    
        ./contrib/scripts/cilium-mesh-smoke-test.sh
    
    This script is idempotent. If there is a need to clean up test
    workloads, then
    
        ./contrib/scripts/cilium-mesh-smoke-test.sh cleanup
    
    should be used.
    
    For the test itself two gateways (two kind clusters) are attached to the default network:
    
       GW1
           \
            [docker network "kind"]
           /
       GW2
    
    The cilium-mesh-smoke-test.sh test adds several endpoints, services,
    netpolicies, and performs a simple connectivity test between them.
    
    When the testing is done, run
    
        make kind-cilium-mesh-down
    
    Signed-off-by: Anton Protopopov <aspsk@isovalent.com>

---
+++ contrib/scripts/enterprise-cilium-mesh-smoke-test.sh
@@ -0,0 +1,160 @@
+#! /bin/bash -efux
+
+service_ip() {
+	local name="$1"
+	local context="${2:-kind-cluster1}"
+	kubectl --context "$context" get svc "$name" -o json | jq -r '.spec.clusterIP'
+}
+
+die() {
+	echo "$@" >&2
+	exit 1
+}
+
+# A simple cilium mesh service pointing to run=nginx endpoints
+svc_config() {
+	local name="$1"
+	cat <<EOF
+apiVersion: v1
+kind: Service
+metadata:
+  name: $name
+  annotations:
+    io.cilium/global-service: "true"
+    com.isovalent/cilium-mesh: "true"
+  labels:
+    test: "smoke-test"
+spec:
+  type: ClusterIP
+  ports:
+  - port: 80
+  selector:
+    run: $name
+EOF
+}
+
+# ingress policy for GW2: allow ingress from app=client
+ingress_policy_config() {
+	cat <<EOF
+apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
+metadata:
+  name: ingress-nginx
+  namespace: default
+  labels:
+    test: "smoke-test"
+spec:
+  podSelector:
+    matchLabels:
+      run: nginx
+  policyTypes:
+    - Ingress
+  ingress:
+    - from:
+        - podSelector:
+            matchLabels:
+              app: client
+      ports:
+        - port: 80
+EOF
+}
+
+# egress policy for GW1: allow egress to run=nginx
+egress_policy_config() {
+	cat <<EOF
+apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
+metadata:
+  name: egress-forbidden-fruit
+  labels:
+    test: "smoke-test"
+spec:
+  podSelector: {}
+  policyTypes:
+    - Egress
+  egress:
+    - to:
+        - podSelector:
+            matchLabels:
+              run: nginx
+      ports:
+        - port: 80
+EOF
+}
+
+#
+# Check that versions are ok
+#
+cilium_cli_ver=$(cilium version | head -1 | cut -f2 -d.)
+[ -n "$cilium_cli_ver" -a "$cilium_cli_ver" -ge "15" ] ||
+	die "cilium-cli version should be >= v0.15"
+
+# We only want to cleanup everything
+if [ -n "${1:-}" -a "${1:-}" = "cleanup" ]; then
+
+	for context in kind-cluster1 kind-cluster2; do
+		kubectl --context "$context" delete netpol -l test=smoke-test
+		kubectl --context "$context" delete svc -l test=smoke-test
+		kubectl --context "$context" delete ime -l test=smoke-test
+	done
+
+	# delete containers by hands, sync with ep-add
+	docker rm -f client
+	docker rm -f clientbad
+	docker rm -f server1
+	docker rm -f server2
+	docker rm -f forbidden-fruit
+
+	exit 0
+fi
+
+#
+# good client, allowed to reach nginx
+#
+contrib/scripts/enterprise-kind-cilium-mesh.sh ep-add 1 client ubuntu app=client,test=smoke-test
+
+#
+# bad client, not allowed to reach nginx
+#
+contrib/scripts/enterprise-kind-cilium-mesh.sh ep-add 1 clientbad ubuntu app=clientbad,test=smoke-test
+
+#
+# nginx backends and services
+#
+contrib/scripts/enterprise-kind-cilium-mesh.sh ep-add 2 server1 nginx run=nginx,test=smoke-test
+contrib/scripts/enterprise-kind-cilium-mesh.sh ep-add 2 server2 nginx run=nginx,test=smoke-test
+svc_config nginx | kubectl --context kind-cluster1 apply -f -
+svc_config nginx | kubectl --context kind-cluster2 apply -f -
+
+#
+# A forbidden service, clients aren't allowed to reach it (egress policy)
+#
+contrib/scripts/enterprise-kind-cilium-mesh.sh ep-add 2 forbidden-fruit nginx run=forbidden-fruit,test=smoke-test
+svc_config forbidden-fruit | kubectl --context kind-cluster1 apply -f -
+svc_config forbidden-fruit | kubectl --context kind-cluster2 apply -f -
+
+# Network policies:
+#   * allow client -> nginx
+#   * deny bad_client -> nginx [ingress at gw2]
+#   * deny * -> forbidden-fruit [egress at gw1]
+
+egress_policy_config  | kubectl --context kind-cluster1 apply -f -
+ingress_policy_config | kubectl --context kind-cluster2 apply -f -
+
+#
+# Finally test that everything works as expected
+#
+
+nginx_cluster_ip=$(service_ip nginx)
+ff_cluster_ip=$(service_ip forbidden-fruit)
+
+docker exec -ti client     curl --connect-timeout 2.718 ${nginx_cluster_ip} ||
+	die "client should have been able to connect to nginx=${nginx_cluster_ip}"
+docker exec -ti clientbad curl --connect-timeout 3.145 ${nginx_cluster_ip} &&
+	die "clientbad should not have been able to connect to nginx=${nginx_cluster_ip}"
+docker exec -ti client     curl --connect-timeout 1.414 ${ff_cluster_ip}    &&
+	die "client should not have been able to connect to forbidden-fruit=${nginx_cluster_ip}"
+docker exec -ti clientbad curl --connect-timeout 1.618 ${ff_cluster_ip}    &&
+	die "clientbad should not have been able to connect to forbidden-fruit=${nginx_cluster_ip}"
+
+exit 0
