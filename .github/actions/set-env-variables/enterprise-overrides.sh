199ff23ba81ed25d98b172503e987f586db6fc80
Gilberto Bertin <jibi@cilium.io>
2024-01-02 08:36:22 +0100

    workflows: move all enterprise env overrides to enterprise-overrises.sh
    Signed-off-by: Gilberto Bertin <jibi@cilium.io>

--- .github/actions/set-env-variables/enterprise-overrides.sh
+++ .github/actions/set-env-variables/enterprise-overrides.sh
@@ -5,7 +5,19 @@
 
 set -ex
 
+echo "QUAY_ORGANIZATION=isovalent" >> "$GITHUB_ENV"
+echo "QUAY_ORGANIZATION_DEV=isovalent-dev" >> "$GITHUB_ENV"
+echo "CILIUM_HELM_REPO_NAME=isovalent" >> "$GITHUB_ENV"
+echo "CILIUM_HELM_REPO_URL=https://helm.isovalent.com" >> "$GITHUB_ENV"
+echo "CILIUM_CLI_REPO=isovalent/cilium-cli-releases" >> "$GITHUB_ENV"
+
+echo "QUAY_CHARTS_ORGANIZATION_DEV=isovalent-charts-dev" >> "$GITHUB_ENV"
+echo "BRANCH_SUFFIX=-ce" >> "$GITHUB_ENV"
+echo "EGRESS_GATEWAY_HELM_VALUES=--helm-set=egressGateway.enabled=true --helm-set=enterprise.egressGatewayHA.enabled=true" >> "$GITHUB_ENV"
+
 echo "CILIUM_CLI_RELEASE_REPO=isovalent/cilium-cli-releases" >> "$GITHUB_ENV"
 # renovate: datasource=github-releases depName=isovalent/cilium-cli-releases
 CILIUM_CLI_VERSION="v0.15.19-cee.1"
 echo "CILIUM_CLI_VERSION=$CILIUM_CLI_VERSION" >> "$GITHUB_ENV"
+
+echo "PUSH_TO_DOCKER_HUB=false" >> "$GITHUB_ENV"
