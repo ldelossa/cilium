dd7ff26421d2d423888f2b99477473deb8ae3846
Julian Wiedmann <jwi@isovalent.com>
2024-01-04 15:47:03 +0200

    k8s: api: remove unused isovalent.com/v1 CRD SchemaVersion constants
    Everything is actually using the version-independent definitions from
    pkg/k8s/apis/isovalent.com/register.go
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- pkg/k8s/apis/isovalent.com/v1/register.go
+++ pkg/k8s/apis/isovalent.com/v1/register.go
@@ -18,16 +18,6 @@ const (
 	// CustomResourceDefinitionVersion is the current version of the resource
 	CustomResourceDefinitionVersion = "v1"
 
-	// CustomResourceDefinitionSchemaVersion is semver-conformant version of CRD schema
-	// Used to determine if CRD needs to be updated in cluster.
-	//
-	// Maintainers: Run ./Documentation/check-crd-compat-table.sh for each release
-	// Developers: Bump patch for each change in the CRD schema.
-	CustomResourceDefinitionSchemaVersion = "1.0.1"
-
-	// CustomResourceDefinitionSchemaVersionKey is key to label which holds the CRD schema version
-	CustomResourceDefinitionSchemaVersionKey = "com.isovalent.k8s.crd.schema.version"
-
 	// IsovalentEgressGatewayPolicy (IEGP)
 	IEGPluralName      = "isovalentegressgatewaypolicies"
 	IEGPKindDefinition = "IsovalentEgressGatewayPolicy"
