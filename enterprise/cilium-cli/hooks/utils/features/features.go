9347453ebe5a5b41c021f1141e97f1f6376f37b2
Marco Iorio <marco.iorio@isovalent.com>
2024-01-15 10:57:44 +0100

    ent/cli: rework feature detection to avoid duplicate cm retrieval
    Avoid to retrieve twice the cilium-config ConfigMap during the
    enterprise feature detection retrieval process, and skip checking
    if the external DNS proxy pods are present when the feature is
    disabled.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- enterprise/cilium-cli/hooks/utils/features/features.go
+++ enterprise/cilium-cli/hooks/utils/features/features.go
@@ -34,62 +34,41 @@ const (
 )
 
 func Detect(ctx context.Context, ct *check.ConnectivityTest) error {
-	err := extractExternalDNSProxyFeature(ctx, ct)
-	if err != nil {
-		return fmt.Errorf("failed to extract feature %s", CiliumDNSProxyDeployed)
-	}
-
-	err = extractFromConfigMap(ctx, ct)
+	err := extractFromConfigMap(ctx, ct)
 	if err != nil {
 		return err
+	}
+
+	err = extractExternalDNSProxyFeature(ctx, ct)
+	if err != nil {
+		return fmt.Errorf("failed to extract feature %s: %w", CiliumDNSProxyDeployed, err)
 	}
 
 	return nil
 }
 
 func extractExternalDNSProxyFeature(ctx context.Context, ct *check.ConnectivityTest) error {
-	isDeployed, err := detectExternalCiliumDNSProxyFeature(ctx, ct)
-	if err != nil {
-		return err
+	// We already checked whether the external dns proxy is enabled in extractFromConfigMap.
+	if !ct.Features[CiliumDNSProxyDeployed].Enabled {
+		return nil
 	}
 
-	ct.Features[CiliumDNSProxyDeployed] = features.Status{
-		Enabled: isDeployed,
-	}
-
-	return nil
-}
-
-// detectExternalCiliumDNSProxyFeature returns true if cilium-dnsproxy pods are deployed.
-func detectExternalCiliumDNSProxyFeature(ctx context.Context, ct *check.ConnectivityTest) (bool, error) {
 	// Check if pods are deployed.
 	for range ct.Clients() {
 		// cilium-dnsproxy pods are labelled with `k8s-app=ciliumdns-proxy`, let's filter on it.
 		ciliumDNSProxyLabelSelector := fmt.Sprintf("k8s-app=%s", tests.ExternalCiliumDNSProxyName)
 		pods, err := ct.K8sClient().ListPods(ctx, ct.Params().CiliumNamespace, metav1.ListOptions{LabelSelector: ciliumDNSProxyLabelSelector})
 		if err != nil {
-			return false, fmt.Errorf("unable to list %s pods: %w", tests.ExternalCiliumDNSProxyName, err)
+			return fmt.Errorf("unable to list %s pods: %w", tests.ExternalCiliumDNSProxyName, err)
 		}
 
 		if len(pods.Items) == 0 {
-			return false, nil
+			ct.Features[CiliumDNSProxyDeployed] = features.Status{Enabled: false}
+			return nil
 		}
 	}
 
-	// Check if configmap is set to enable external dns proxy.
-	cm, err := ct.K8sClient().GetConfigMap(ctx, ct.Params().CiliumNamespace, defaults.ConfigMapName, metav1.GetOptions{})
-	if err != nil {
-		return false, fmt.Errorf("unable to retrieve ConfigMap %q: %w", defaults.ConfigMapName, err)
-	}
-	if cm.Data == nil {
-		return false, fmt.Errorf("ConfigMap %q does not contain any configuration", defaults.ConfigMapName)
-	}
-
-	if v, ok := cm.Data["external-dns-proxy"]; !ok || v != "true" {
-		return false, nil
-	}
-
-	return true, nil
+	return nil
 }
 
 func extractFromConfigMap(ctx context.Context, ct *check.ConnectivityTest) error {
@@ -105,6 +84,10 @@ func extractFromConfigMap(ctx context.Context, ct *check.ConnectivityTest) error
 		Enabled: cm.Data["enable-ipv4-egress-gateway-ha"] == "true" ||
 			// in Cilium v1.14-ce we auto opt into egress gateway HA in case the OSS feature is enabled, for backward compatibility with 1.13-ce
 			(versioncheck.MustCompile(">=1.14.0 <1.15.0")(ct.CiliumVersion) && cm.Data["enable-ipv4-egress-gateway"] == "true"),
+	}
+
+	ct.Features[CiliumDNSProxyDeployed] = features.Status{
+		Enabled: cm.Data["external-dns-proxy"] == "true",
 	}
 
 	return nil
