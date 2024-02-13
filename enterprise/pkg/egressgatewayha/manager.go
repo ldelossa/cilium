ldelossa/cilium

commit ceb9c84fe27e1d61a5cc2f770f1c77efed8b750b

egressgwha: manager: add support for AZ affinity
this commit adds support to the egressgwha agent manager for AZ
affinity.

With this change, whenever AZ affinity is enabled for a given
IsovalentEgressGatewayPolicy, the manager will first retrieve the AZ of
such endpoint:

    CiliumEndpoint -> node -> "topology.kubernetes.io/zone" node's label

and then use that to select the list of active gateways for the
endpoint's zone, from the groupStatuses[].activeGatewayIPsByAZ status
field of the policy

Signed-off-by: Gilberto Bertin <jibi@cilium.io>

enterprise/pkg/egressgatewayha/manager.go
@@ -41,6 +41,7 @@ import (
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/node"
+	"github.com/cilium/cilium/pkg/node/addressing"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 	"github.com/cilium/cilium/pkg/option"
 	"github.com/cilium/cilium/pkg/time"
@@ -130,6 +131,9 @@ type Manager struct {
 
 	// nodeDataStore stores node names to node mapping
 	nodeDataStore map[string]nodeTypes.Node
+
+	// nodesByIP stores node IPs to node mapping
+	nodesByIP map[string]nodeTypes.Node
 
 	// policyConfigs stores policy configs indexed by policyID
 	policyConfigs map[policyID]*PolicyConfig
@@ -584,6 +588,18 @@ func (manager *Manager) updatePoliciesBySourceIP() {
 	}
 }
 
+func (manager *Manager) updateNodesByIP() {
+	manager.nodesByIP = make(map[string]nodeTypes.Node)
+
+	for _, node := range manager.nodeDataStore {
+		for _, ipAddress := range node.IPAddresses {
+			if ipAddress.AddrType() == addressing.NodeInternalIP {
+				manager.nodesByIP[ipAddress.ToString()] = node
+			}
+		}
+	}
+}
+
 // policyMatches returns true if there exists at least one policy matching the
 // given parameters.
 //
@@ -625,7 +641,7 @@ func (manager *Manager) addMissingEgressRules() {
 		})
 
 	addEgressRule := func(endpoint *endpointMetadata, dstCIDR netip.Prefix, excludedCIDR bool, gwc *gatewayConfig) {
-		activeGatewayIPs := gwc.activeGatewayIPs
+		activeGatewayIPs, egressIP := gwc.gatewayConfigForEndpoint(manager, endpoint)
 		if excludedCIDR {
 			activeGatewayIPs = []netip.Addr{ExcludedCIDRIPv4}
 		}
@@ -634,18 +650,18 @@ func (manager *Manager) addMissingEgressRules() {
 			policyKey := egressmapha.NewEgressPolicyKey4(endpointIP, dstCIDR)
 			policyVal, policyPresent := egressPolicies[policyKey]
 
-			if policyPresent && policyVal.Match(gwc.egressIP, activeGatewayIPs) {
+			if policyPresent && policyVal.Match(egressIP, activeGatewayIPs) {
 				return
 			}
 
 			logger := log.WithFields(logrus.Fields{
 				logfields.SourceIP:        endpointIP,
 				logfields.DestinationCIDR: dstCIDR.String(),
-				logfields.EgressIP:        gwc.egressIP,
+				logfields.EgressIP:        egressIP,
 				logfields.GatewayIPs:      joinStringers(activeGatewayIPs, ","),
 			})
 
-			if err := egressmapha.ApplyEgressPolicy(manager.policyMap, endpointIP, dstCIDR, gwc.egressIP, activeGatewayIPs); err != nil {
+			if err := egressmapha.ApplyEgressPolicy(manager.policyMap, endpointIP, dstCIDR, egressIP, activeGatewayIPs); err != nil {
 				logger.WithError(err).Error("Error applying egress gateway policy")
 			} else {
 				logger.Debug("Egress gateway policy applied")
@@ -670,13 +686,13 @@ func (manager *Manager) removeUnusedEgressRules() {
 nextPolicyKey:
 	for policyKey, policyVal := range egressPolicies {
 		matchPolicy := func(endpoint *endpointMetadata, dstCIDR netip.Prefix, excludedCIDR bool, gwc *gatewayConfig) bool {
-			activeGatewayIPs := gwc.activeGatewayIPs
+			activeGatewayIPs, egressIP := gwc.gatewayConfigForEndpoint(manager, endpoint)
 			if excludedCIDR {
 				activeGatewayIPs = []netip.Addr{ExcludedCIDRIPv4}
 			}
 
 			for _, endpointIP := range endpoint.ips {
-				if policyKey.Match(endpointIP, dstCIDR) && policyVal.Match(gwc.egressIP, activeGatewayIPs) {
+				if policyKey.Match(endpointIP, dstCIDR) && policyVal.Match(egressIP, activeGatewayIPs) {
 					return true
 				}
 			}
@@ -781,6 +797,9 @@ func (manager *Manager) reconcileLocked() {
 		fallthrough
 	case manager.eventBitmapIsSet(eventAddPolicy, eventDeletePolicy):
 		manager.updatePoliciesBySourceIP()
+		fallthrough
+	case manager.eventBitmapIsSet(eventUpdateNode, eventDeleteNode):
+		manager.updateNodesByIP()
 	}
 
 	manager.regenerateGatewayConfigs()

enterprise/pkg/egressgatewayha/endpoint.go
@@ -22,6 +22,8 @@ type endpointMetadata struct {
 	id endpointID
 	// ips are endpoint's unique IPs
 	ips []netip.Addr
+	// nodeIP is the internal IP of the node where the endpoint's pod is running
+	nodeIP netip.Addr
 }
 
 // endpointID is based on endpoint's UID
@@ -57,10 +59,16 @@ func getEndpointMetadata(endpoint *k8sTypes.CiliumEndpoint, identityLabels label
 		return nil, fmt.Errorf("endpoint has no identity metadata")
 	}
 
+	nodeIP, err := netip.ParseAddr(endpoint.Networking.NodeIP)
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse endpoint's node IP")
+	}
+
 	data := &endpointMetadata{
-		ips:    addrs,
 		labels: identityLabels.K8sStringMap(),
 		id:     endpoint.UID,
+		ips:    addrs,
+		nodeIP: nodeIP,
 	}
 
 	return data, nil

enterprise/pkg/egressgatewayha/helpers_test.go
@@ -93,17 +93,18 @@ func addIEGP(tb testing.TB, policies fakeResource[*Policy], policy *v1.Isovalent
 }
 
 type policyParams struct {
-	name              string
-	endpointLabels    map[string]string
-	destinationCIDR   string
-	excludedCIDRs     []string
-	nodeLabels        map[string]string
-	iface             string
-	egressIP          string
-	maxGatewayNodes   int
-	azAffinity        azAffinityMode
-	activeGatewayIPs  []string
-	healthyGatewayIPs []string
+	name                 string
+	endpointLabels       map[string]string
+	destinationCIDR      string
+	excludedCIDRs        []string
+	nodeLabels           map[string]string
+	iface                string
+	egressIP             string
+	maxGatewayNodes      int
+	azAffinity           azAffinityMode
+	activeGatewayIPs     []string
+	activeGatewayIPsByAZ map[string][]string
+	healthyGatewayIPs    []string
 }
 
 func newIEGP(params *policyParams) (*Policy, *PolicyConfig) {
@@ -120,6 +121,13 @@ func newIEGP(params *policyParams) (*Policy, *PolicyConfig) {
 	parsedActiveGatewayIPs := []netip.Addr{}
 	for _, activeGatewayIP := range params.activeGatewayIPs {
 		parsedActiveGatewayIPs = append(parsedActiveGatewayIPs, netip.MustParseAddr(activeGatewayIP))
+	}
+
+	parsedActiveGatewayIPsByAZ := map[string][]netip.Addr{}
+	for az, activeGatewayIPs := range params.activeGatewayIPsByAZ {
+		for _, activeGatewayIP := range activeGatewayIPs {
+			parsedActiveGatewayIPsByAZ[az] = append(parsedActiveGatewayIPsByAZ[az], netip.MustParseAddr(activeGatewayIP))
+		}
 	}
 
 	parsedHealthyGatewayIPs := []netip.Addr{}
@@ -154,8 +162,9 @@ func newIEGP(params *policyParams) (*Policy, *PolicyConfig) {
 		},
 		groupStatuses: []groupStatus{
 			{
-				activeGatewayIPs:  parsedActiveGatewayIPs,
-				healthyGatewayIPs: parsedHealthyGatewayIPs,
+				activeGatewayIPs:     parsedActiveGatewayIPs,
+				activeGatewayIPsByAZ: parsedActiveGatewayIPsByAZ,
+				healthyGatewayIPs:    parsedHealthyGatewayIPs,
 			},
 		},
 	}
@@ -207,8 +216,9 @@ func newIEGP(params *policyParams) (*Policy, *PolicyConfig) {
 		Status: v1.IsovalentEgressGatewayPolicyStatus{
 			GroupStatuses: []v1.IsovalentEgressGatewayPolicyGroupStatus{
 				{
-					ActiveGatewayIPs:  params.activeGatewayIPs,
-					HealthyGatewayIPs: params.healthyGatewayIPs,
+					ActiveGatewayIPs:     params.activeGatewayIPs,
+					ActiveGatewayIPsByAZ: params.activeGatewayIPsByAZ,
+					HealthyGatewayIPs:    params.healthyGatewayIPs,
 				},
 			},
 		},

enterprise/pkg/egressgatewayha/manager_privileged_test.go
@@ -24,6 +24,7 @@ import (
 	"github.com/cilium/cilium/pkg/hive/hivetest"
 	"github.com/cilium/cilium/pkg/identity"
 	cilium_api_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/k8s/resource"
 	slimv1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
 	k8sTypes "github.com/cilium/cilium/pkg/k8s/types"
 	"github.com/cilium/cilium/pkg/labels"
@@ -274,7 +275,7 @@ func (k *EgressGatewayTestSuite) TestEgressGatewayManagerHAGroup(c *C) {
 	assertEgressRules(c, policyMap, []egressRule{})
 
 	// Add a new endpoint which matches policy-1
-	ep1, id1 := newEndpointAndIdentity("ep-1", ep1IP, ep1Labels)
+	ep1, id1 := newEndpointAndIdentity("ep-1", ep1IP, ep1Labels, node1IP)
 	addEndpoint(c, k.endpoints, &ep1)
 	reconciliationEventsCount = waitForReconciliationRun(c, egressGatewayManager, reconciliationEventsCount)
 
@@ -371,7 +372,7 @@ func (k *EgressGatewayTestSuite) TestEgressGatewayManagerHAGroup(c *C) {
 	})
 
 	// Add a new endpoint that matches policy-2
-	ep2, id2 := newEndpointAndIdentity("ep-2", ep2IP, ep2Labels)
+	ep2, id2 := newEndpointAndIdentity("ep-2", ep2IP, ep2Labels, node1IP)
 	addEndpoint(c, k.endpoints, &ep2)
 	reconciliationEventsCount = waitForReconciliationRun(c, egressGatewayManager, reconciliationEventsCount)
 
@@ -464,6 +465,102 @@ func (k *EgressGatewayTestSuite) TestEgressGatewayManagerHAGroup(c *C) {
 	assertEgressRules(c, policyMap, []egressRule{})
 }
 
+func (k *EgressGatewayTestSuite) TestEgressGatewayManagerHAGroupAZAffinity(c *C) {
+	createTestInterface(c, testInterface1, egressCIDR1)
+	createTestInterface(c, testInterface2, egressCIDR2)
+
+	policyMap := k.manager.policyMap
+	egressGatewayManager := k.manager
+	reconciliationEventsCount := egressGatewayManager.reconciliationEventsCount.Load()
+
+	k.policies.sync(c)
+	k.endpoints.sync(c)
+	k.ciliumNodes.sync(c)
+
+	reconciliationEventsCount = waitForReconciliationRun(c, egressGatewayManager, reconciliationEventsCount)
+
+	node1 := newCiliumNode(node1Name, node1IP, nodeGroup1LabelsAZ1)
+	k.ciliumNodes.process(c, resource.Event[*cilium_api_v2.CiliumNode]{
+		Kind:   resource.Upsert,
+		Object: node1.ToCiliumNode(),
+	})
+	reconciliationEventsCount = waitForReconciliationRun(c, egressGatewayManager, reconciliationEventsCount)
+
+	node2 := newCiliumNode(node2Name, node2IP, nodeGroup1LabelsAZ2)
+	k.ciliumNodes.process(c, resource.Event[*cilium_api_v2.CiliumNode]{
+		Kind:   resource.Upsert,
+		Object: node2.ToCiliumNode(),
+	})
+	reconciliationEventsCount = waitForReconciliationRun(c, egressGatewayManager, reconciliationEventsCount)
+
+	// Create a new HA policy that selects k8s1 and k8s2 nodes
+	policy1 := &policyParams{
+		name:             "policy-1",
+		endpointLabels:   ep1Labels,
+		destinationCIDR:  destCIDR,
+		azAffinity:       azAffinityLocalOnly,
+		nodeLabels:       nodeGroup1Labels,
+		iface:            testInterface1,
+		activeGatewayIPs: []string{node1IP, node2IP},
+		activeGatewayIPsByAZ: map[string][]string{
+			"az-1": {node1IP},
+			"az-2": {node2IP},
+		},
+		healthyGatewayIPs: []string{node1IP, node2IP},
+	}
+	addPolicy(c, k.policies, policy1)
+	reconciliationEventsCount = waitForReconciliationRun(c, egressGatewayManager, reconciliationEventsCount)
+
+	assertEgressRules(c, policyMap, []egressRule{})
+
+	// Add a new endpoint on node-1 which matches policy-1
+	ep1, _ := newEndpointAndIdentity("ep-1", ep1IP, ep1Labels, node1IP)
+	addEndpoint(c, k.endpoints, &ep1)
+	reconciliationEventsCount = waitForReconciliationRun(c, egressGatewayManager, reconciliationEventsCount)
+
+	assertEgressRules(c, policyMap, []egressRule{
+		{ep1IP, destCIDR, egressIP1, node1IP},
+	})
+
+	// Add a new endpoint on node-2 which matches policy-1
+	ep2, _ := newEndpointAndIdentity("ep-2", ep2IP, ep1Labels, node2IP)
+	addEndpoint(c, k.endpoints, &ep2)
+	reconciliationEventsCount = waitForReconciliationRun(c, egressGatewayManager, reconciliationEventsCount)
+
+	assertEgressRules(c, policyMap, []egressRule{
+		{ep1IP, destCIDR, egressIP1, node1IP},
+		{ep2IP, destCIDR, zeroIP4, node2IP},
+	})
+
+	// Remove k8s1
+	policy1.activeGatewayIPs = []string{node2IP}
+	policy1.activeGatewayIPsByAZ = map[string][]string{
+		"az-2": {node2IP},
+	}
+	policy1.healthyGatewayIPs = []string{node2IP}
+	addPolicy(c, k.policies, policy1)
+	reconciliationEventsCount = waitForReconciliationRun(c, egressGatewayManager, reconciliationEventsCount)
+
+	assertEgressRules(c, policyMap, []egressRule{
+		{ep2IP, destCIDR, zeroIP4, node2IP},
+	})
+
+	// Add back node1
+	policy1.activeGatewayIPsByAZ = map[string][]string{
+		"az-1": {node1IP},
+		"az-2": {node2IP},
+	}
+	policy1.activeGatewayIPs = []string{node1IP, node2IP}
+	policy1.healthyGatewayIPs = []string{node1IP, node2IP}
+	addPolicy(c, k.policies, policy1)
+	waitForReconciliationRun(c, egressGatewayManager, reconciliationEventsCount)
+
+	assertEgressRules(c, policyMap, []egressRule{
+		{ep1IP, destCIDR, egressIP1, node1IP},
+		{ep2IP, destCIDR, zeroIP4, node2IP},
+	})
+}
+
 func (k *EgressGatewayTestSuite) TestEgressGatewayManagerCtEntries(c *C) {
 	createTestInterface(c, testInterface1, egressCIDR1)
 
@@ -493,7 +590,7 @@ func (k *EgressGatewayTestSuite) TestEgressGatewayManagerCtEntries(c *C) {
 	assertEgressCtEntries(c, k.manager.ctMap, []egressCtEntry{})
 
 	// Add a new endpoint which matches policy-1
-	ep1, _ := newEndpointAndIdentity("ep-1", ep1IP, ep1Labels)
+	ep1, _ := newEndpointAndIdentity("ep-1", ep1IP, ep1Labels, node1IP)
 	addEndpoint(c, k.endpoints, &ep1)
 	reconciliationEventsCount = waitForReconciliationRun(c, egressGatewayManager, reconciliationEventsCount)
 
@@ -791,7 +888,7 @@ func (k *EgressGatewayTestSuite) TestEndpointDataStore(c *C) {
 	assertEgressRules(c, policyMap, []egressRule{})
 
 	// Add a new endpoint & ID which matches policy-1
-	ep1, _ := newEndpointAndIdentity("ep-1", ep1IP, ep1Labels)
+	ep1, _ := newEndpointAndIdentity("ep-1", ep1IP, ep1Labels, node1IP)
 	addEndpoint(c, k.endpoints, &ep1)
 	reconciliationEventsCount = waitForReconciliationRun(c, egressGatewayManager, reconciliationEventsCount)
 
@@ -804,7 +901,7 @@ func (k *EgressGatewayTestSuite) TestEndpointDataStore(c *C) {
 
 	// Produce a new endpoint ep2 similar to ep1 - with the same name & labels, but with a different IP address.
 	// The ep1 will be deleted.
-	ep2, _ := newEndpointAndIdentity(ep1.Name, ep2IP, ep1Labels)
+	ep2, _ := newEndpointAndIdentity(ep1.Name, ep2IP, ep1Labels, node1IP)
 
 	// Test event order: add new -> delete old
 	addEndpoint(c, k.endpoints, &ep2)
@@ -818,7 +915,7 @@ func (k *EgressGatewayTestSuite) TestEndpointDataStore(c *C) {
 	})
 
 	// Produce a new endpoint ep3 similar to ep2 (and ep1) - with the same name & labels, but with a different IP address.
-	ep3, _ := newEndpointAndIdentity(ep1.Name, ep3IP, ep1Labels)
+	ep3, _ := newEndpointAndIdentity(ep1.Name, ep3IP, ep1Labels, node1IP)
 
 	// Test event order: delete old -> update new
 	deleteEndpoint(c, k.endpoints, &ep2)
@@ -896,7 +993,7 @@ func newCiliumNode(name, nodeIP string, nodeLabels map[string]string) nodeTypes.
 }
 
 // Mock the creation of endpoint and its corresponding identity, returns endpoint and ID.
-func newEndpointAndIdentity(name, ip string, epLabels map[string]string) (k8sTypes.CiliumEndpoint, *identity.Identity) {
+func newEndpointAndIdentity(name, ip string, epLabels map[string]string, nodeIP string) (k8sTypes.CiliumEndpoint, *identity.Identity) {
 	id, _, _ := identityAllocator.AllocateIdentity(context.Background(), labels.Map2Labels(epLabels, labels.LabelSourceK8s), true, identity.InvalidIdentity)
 
 	return k8sTypes.CiliumEndpoint{
@@ -913,6 +1010,7 @@ func newEndpointAndIdentity(name, ip string, epLabels map[string]string) (k8sTyp
 					IPV4: ip,
 				},
 			},
+			NodeIP: nodeIP,
 		},
 	}, id
 }

enterprise/pkg/egressgatewayha/policy.go
@@ -46,6 +46,22 @@ type groupConfig struct {
 	maxGatewayNodes int
 }
 
+// azActiveGatewayIPs is a list of active gateway IPs for a particular AZ.
+// In addition to the list of IPs, localNodeConfiguredAsGateway specifies if the
+// local node is configured as a gateway for the AZ.
+type azActiveGatewayIPs struct {
+	// list of active gateway IPs for a given AZ
+	gatewayIPs []netip.Addr
+
+	// with AZ affinity enabled, within an egress group, the same node can be
+	// be configured as gateway for some endpoints, while being not enabled
+	// for others.
+	//
+	// We track this information to determine correctly the egress IP to
+	// use for a given endpoint.
+	localNodeConfiguredAsGateway bool
+}
+
 // gatewayConfig is the gateway configuration derived at runtime from a policy.
 //
 // Some of these fields are derived from the running system as the policy may
@@ -55,6 +71,7 @@ type groupConfig struct {
 type gatewayConfig struct {
 	// ifaceName is the name of the interface used to SNAT traffic
 	ifaceName string
+
 	// egressIP is the IP used to SNAT traffic
 	egressIP netip.Addr
 
@@ -62,11 +79,18 @@ type gatewayConfig struct {
 	// egress gateways
 	activeGatewayIPs []netip.Addr
 
+	// activeGatewayIPsByAZ maps AZs to a slice of node IPs that are
+	// actively working as egress gateway for that AZ
+	activeGatewayIPsByAZ map[string]azActiveGatewayIPs
+
 	// healthyGatewayIPs is the entire pool of healthy nodes that can act as
 	// egress gateway for the given policy.
 	// Not all of them may be actively acting as gateway since with the
 	// maxGatewayNodes policy directive we can select a subset of them
 	healthyGatewayIPs []netip.Addr
+
+	// azAffinity configures the AZ affinity mode for the policy
+	azAffinity azAffinityMode
 
 	// localNodeConfiguredAsGateway tells if the local node belongs to the
 	// pool of egress gateway node for this config.
@@ -404,9 +428,11 @@ func (config *PolicyConfig) updateGroupStatuses(operatorManager *OperatorManager
 
 func (config *PolicyConfig) regenerateGatewayConfig(manager *Manager) {
 	config.gatewayConfig = gatewayConfig{
-		egressIP:          netip.IPv4Unspecified(),
-		activeGatewayIPs:  []netip.Addr{},
-		healthyGatewayIPs: []netip.Addr{},
+		egressIP:             netip.IPv4Unspecified(),
+		activeGatewayIPs:     []netip.Addr{},
+		activeGatewayIPsByAZ: map[string]azActiveGatewayIPs{},
+		healthyGatewayIPs:    []netip.Addr{},
+		azAffinity:           config.azAffinity,
 	}
 
 	if len(config.groupStatuses) == 0 {
@@ -429,28 +455,48 @@ func (config *PolicyConfig) regenerateGatewayConfig(manager *Manager) {
 	for groupIndex, gc := range config.groupConfigs {
 		groupStatus := &config.groupStatuses[groupIndex]
 
-		gwc.activeGatewayIPs = append(gwc.activeGatewayIPs, groupStatus.activeGatewayIPs...)
+		if !gwc.azAffinity.enabled() {
+			gwc.activeGatewayIPs = append(gwc.activeGatewayIPs, groupStatus.activeGatewayIPs...)
+		} else {
+			for az, gwIPs := range groupStatus.activeGatewayIPsByAZ {
+				azGwIPs, ok := gwc.activeGatewayIPsByAZ[az]
+				if !ok {
+					azGwIPs = azActiveGatewayIPs{
+						gatewayIPs: []netip.Addr{},
+					}
+				}
+				azGwIPs.gatewayIPs = append(azGwIPs.gatewayIPs, gwIPs...)
+				gwc.activeGatewayIPsByAZ[az] = azGwIPs
+			}
+		}
 		gwc.healthyGatewayIPs = append(gwc.healthyGatewayIPs, groupStatus.healthyGatewayIPs...)
 
 		// We use the local node IP to determine if the current node
 		// matches the list of active gateway IPs
-		if slices.Contains(groupStatus.activeGatewayIPs, localNodeK8sAddr) {
-			logger := log.WithFields(logrus.Fields{
-				logfields.IsovalentEgressGatewayPolicyName: config.id,
-				logfields.Interface:                        gc.iface,
-				logfields.EgressIP:                         gc.egressIP,
-			})
+		localNodeMatchesGatewayIP := false
 
-			// If localNodeConfiguredAsGateway is already set it means that another
-			// egress group for the same policy has already selected it as gateway. In
-			// this case don't regenerate a new gatewayConfig and emit a warning
-			if gwc.localNodeConfiguredAsGateway {
-				logger.Warning("Local node selected by multiple egress gateway groups from the same policy")
-				continue
+		if !gwc.azAffinity.enabled() {
+			if slices.Contains(gwc.activeGatewayIPs, localNodeK8sAddr) {
+				localNodeMatchesGatewayIP = true
 			}
+		} else {
+			for az, azActiveGatewayIPs := range gwc.activeGatewayIPsByAZ {
+				if slices.Contains(azActiveGatewayIPs.gatewayIPs, localNodeK8sAddr) {
+					localNodeMatchesGatewayIP = true
 
+					azActiveGatewayIPs.localNodeConfiguredAsGateway = true
+					gwc.activeGatewayIPsByAZ[az] = azActiveGatewayIPs
+				}
+			}
+		}
+
+		if localNodeMatchesGatewayIP {
 			if err := gwc.deriveFromGroupConfig(&gc); err != nil {
-				logger.WithError(err).Error("Failed to derive policy gateway configuration")
+				log.WithFields(logrus.Fields{
+					logfields.IsovalentEgressGatewayPolicyName: config.id,
+					logfields.Interface:                        gc.iface,
+					logfields.EgressIP:                         gc.egressIP,
+				}).WithError(err).Error("Failed to derive policy gateway configuration")
 			}
 		}
 	}
@@ -459,8 +505,14 @@ func (config *PolicyConfig) regenerateGatewayConfig(manager *Manager) {
 // deriveFromGroupConfig retrieves all the missing gateway configuration data
 // (such as egress IP or interface) given a policy group config
 func (gwc *gatewayConfig) deriveFromGroupConfig(gc *groupConfig) error {
-	var err error
+	// If localNodeConfiguredAsGateway is already set it means that another
+	// egress group for the same policy has already selected it as gateway. In
+	// this case don't regenerate a new gatewayConfig and return an error
+	if gwc.localNodeConfiguredAsGateway {
+		return fmt.Errorf("local node selected by multiple egress gateway groups from the same policy")
+	}
 
+	var err error
 	gwc.localNodeConfiguredAsGateway = false
 
 	switch {
@@ -498,6 +550,49 @@ func (gwc *gatewayConfig) deriveFromGroupConfig(gc *groupConfig) error {
 	gwc.localNodeConfiguredAsGateway = true
 
 	return nil
+}
+
+// gatewayConfigForEndpoint returns the configuration of active gateway IPs
+// and egress IP for a given endpoint
+//
+// If the AZ resolution fails, this method will fallback to the non AZ-aware list of active gateway IPs
+func (gwc *gatewayConfig) gatewayConfigForEndpoint(manager *Manager, endpoint *endpointMetadata) ([]netip.Addr, netip.Addr) {
+	egressIP := netip.IPv4Unspecified()
+	if gwc.localNodeConfiguredAsGateway {
+		egressIP = gwc.egressIP
+	}
+
+	if !gwc.azAffinity.enabled() {
+		return gwc.activeGatewayIPs, egressIP
+	}
+
+	logger := log.WithFields(logrus.Fields{
+		logfields.EndpointID: endpoint.id,
+		logfields.K8sNodeIP:  endpoint.nodeIP.String,
+	})
+
+	endpointNode, ok := manager.nodesByIP[endpoint.nodeIP.String()]
+	if !ok {
+		logger.Error("cannot find endpoint's node")
+
+		//fallback to the non AZ-aware list of gateway IPs
+		return gwc.activeGatewayIPs, egressIP
+	}
+
+	az, ok := endpointNode.Labels[core_v1.LabelTopologyZone]
+	if !ok {
+		logger.Errorf("missing node's AZ label")
+
+		//fallback to the non AZ-aware list of gateway IPs
+		return gwc.activeGatewayIPs, egressIP
+	}
+
+	egressIP = netip.IPv4Unspecified()
+	if gwc.activeGatewayIPsByAZ[az].localNodeConfiguredAsGateway {
+		egressIP = gwc.egressIP
+	}
+
+	return gwc.activeGatewayIPsByAZ[az].gatewayIPs, egressIP
 }
 
 // forEachEndpointAndCIDR iterates through each combination of endpoints and

commit 285d86db96a2b623c22f2cfc6bf4e4eac361613c

egressgwha: manager: run updateNodesByIP() only with node events
Fixes: ceb9c84fe2 ("egressgwha: manager: add support for AZ affinity")
Signed-off-by: Gilberto Bertin <jibi@cilium.io>

enterprise/pkg/egressgatewayha/manager.go
@@ -787,18 +787,24 @@ func (manager *Manager) reconcileLocked() {
 		return
 	}
 
-	switch {
 	// on eventK8sSyncDone we need to update all caches unconditionally as
 	// we don't know which k8s events/resources were received during the
 	// initial k8s sync
-	case manager.eventBitmapIsSet(eventUpdateEndpoint, eventDeleteEndpoint, eventK8sSyncDone):
+	if manager.eventBitmapIsSet(eventK8sSyncDone) {
 		manager.updatePoliciesMatchedEndpointIDs()
-		fallthrough
-	case manager.eventBitmapIsSet(eventAddPolicy, eventDeletePolicy):
 		manager.updatePoliciesBySourceIP()
-		fallthrough
-	case manager.eventBitmapIsSet(eventUpdateNode, eventDeleteNode):
 		manager.updateNodesByIP()
+	} else {
+		if manager.eventBitmapIsSet(eventUpdateEndpoint, eventDeleteEndpoint, eventAddPolicy, eventDeletePolicy) {
+			if manager.eventBitmapIsSet(eventUpdateEndpoint, eventDeleteEndpoint) {
+				manager.updatePoliciesMatchedEndpointIDs()
+			}
+			manager.updatePoliciesBySourceIP()
+		}
+
+		if manager.eventBitmapIsSet(eventUpdateNode, eventDeleteNode) {
+			manager.updateNodesByIP()
+		}
 	}
 
 	manager.regenerateGatewayConfigs()