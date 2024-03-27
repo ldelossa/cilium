ldelossa/cilium

commit ca0ef85546b42a3b78cfaafcfefb517a19e40342

mixed routing: filter ipset entries creation based on routing mode
Normally, Cilium inserts an ipset entry for each NodeInternalIP address
when configured in native routing mode if iptables masquerading is
enabled, to skip masquerading for pod to node traffic. Differently,
masquerading must be performed when running in tunnel mode, otherwise
packets may be dropped, and return traffic would not work.

This commit extends the mixed routing node manager to additionally
filter out insertions into the ipset for the InternalIP addresses
of nodes configured in tunneling mode, in case the primary routing
mode is set to native (which would normally cause the configuration
of the ipset entries). Specifically, this is achieved overriding the
IPSetManager leveraged by the OSS logic with a wrapper, which takes
into account the exclusion list configured when new node upsertions
and deletions are observed. ipset deletions are assumed to be no-op
if the entry is not already present, and are always propagated.
Additionally, possibly existing stale ipset entries are deleted
if the corresponding insertion was filtered out.

The logic herein introduced works under the assumption that the
fallback routing mode is tunnel (i.e., the only supported configuration
at the moment). Hence, it filters out the ipset entries associated
with nodes towards which the preferred routing mode is tunnel.
To support native routing fallback, instead, we should force the
creation of the ipset, and manually insert the appropriate entries.

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

enterprise/pkg/mixedrouting/mixedrouting.go
@@ -19,6 +19,7 @@ import (
 	"github.com/sirupsen/logrus"
 
 	"github.com/cilium/cilium/pkg/clustermesh"
+	"github.com/cilium/cilium/pkg/datapath/iptables"
 	linuxdatapath "github.com/cilium/cilium/pkg/datapath/linux"
 	"github.com/cilium/cilium/pkg/datapath/tunnel"
 	datapath "github.com/cilium/cilium/pkg/datapath/types"
@@ -83,7 +84,8 @@ type params struct {
 	DaemonConfig *option.DaemonConfig
 	Tunnel       tunnel.Config
 
-	NodeManager nodemanager.NodeManager
+	NodeManager     nodemanager.NodeManager
+	IPTablesManager *iptables.Manager
 }
 
 func newManager(in params) *manager {
@@ -101,6 +103,7 @@ func newManager(in params) *manager {
 		logger:     mgr.logger,
 		modes:      mgr.modes,
 		downstream: in.NodeManager,
+		ipsetter:   in.IPTablesManager,
 	}
 
 	return &mgr
@@ -116,7 +119,7 @@ func (mgr *manager) configureLocalNode(lns *node.LocalNodeStore) {
 	})
 }
 
-func (mgr *manager) setupNodeManager(dp datapath.Datapath, cm *clustermesh.ClusterMesh) {
+func (mgr *manager) setupNodeManager(dp datapath.Datapath, cm *clustermesh.ClusterMesh, nomgr nodemanager.NodeManager) {
 	// We don't need to hook the extra logic if the local node is configured
 	// with a single routing mode, as it will be always selected anyway.
 	// However, we still inject the lightweight version of the node manager
@@ -128,6 +131,15 @@ func (mgr *manager) setupNodeManager(dp datapath.Datapath, cm *clustermesh.Clust
 
 	clustermesh.InjectCENodeObserver(cm, mgr.nodes)
 	linuxdatapath.InjectCEEnableEncapsulation(dp, mgr.nodes.needsEncapsulation)
+
+	// Note: the current approach works assuming that the fallback routing mode
+	// is tunnel (the only supported configuration at the moment), by filtering
+	// out the ipset insertions/deletions if the preferred routing mode towards
+	// the given node is tunneling (and the local routing mode is set to native,
+	// otherwise they would not have been inserted in the first place). To support
+	// native routing fallback, instead, we should force the creation of the ipset
+	// and manually insert the appropriate entries.
+	nodemanager.InjectCEIPSetManager(nomgr, mgr.nodes)
 }
 
 // enabled returns whether mixed routing mode support is enabled.

enterprise/pkg/mixedrouting/node.go
@@ -11,10 +11,14 @@
 package mixedrouting
 
 import (
+	"net"
+	"slices"
+
 	"github.com/sirupsen/logrus"
 
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	nodemanager "github.com/cilium/cilium/pkg/node/manager"
 	"github.com/cilium/cilium/pkg/node/store"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 )
@@ -25,6 +29,9 @@ type nodeManager struct {
 
 	downstream store.NodeManager
 	nodesCache lock.Map[nodeTypes.Identity, *nodeTypes.Node]
+
+	ipsetter  nodemanager.CEIPSetManager
+	ipsetSkip lock.Map[string, struct{}]
 }
 
 // NodeUpdated wraps the corresponding nodemanager.Manager method to observe node
@@ -49,10 +56,13 @@ func (nm *nodeManager) NodeUpdated(node nodeTypes.Node) {
 		log.Warning("Preferred routing mode changed. " +
 			"Expect connectivity disruption towards hosted endpoints")
 		nm.NodeDeleted(*prev)
+		prev = nil
 	}
 
 	log.Debug("Observed node upsertion")
 	nm.nodesCache.Store(id, &node)
+
+	nm.updateIpsetSkipSet(prev, &node, log)
 
 	nm.downstream.NodeUpdated(node)
 }
@@ -70,8 +80,33 @@ func (nm *nodeManager) NodeDeleted(node nodeTypes.Node) {
 	)
 
 	log.Debug("Observed node deletion")
-	nm.downstream.NodeDeleted(node)
 	nm.nodesCache.Delete(id)
+
+	nm.updateIpsetSkipSet(&node, nil, log)
+
+	nm.downstream.NodeDeleted(node)
+}
+
+// AddToNodeIpset wraps the corresponding iptables.Manager method, skipping the
+// insertion if the IP address belongs to the exclusion list (i.e., it was
+// configured to prefer tunnel routing).
+func (nm *nodeManager) AddToNodeIpset(nodeIP net.IP) {
+	ipstr := nodeIP.String()
+	if _, ok := nm.ipsetSkip.Load(ipstr); ok {
+		nm.logger.WithField(logfields.IPAddr, ipstr).
+			Debug("Skipping ipset insertion, as the IP belongs to the exclusion list")
+
+		// Ensure that no stale entry is present.
+		nm.RemoveFromNodeIpset(nodeIP)
+		return
+	}
+
+	nm.ipsetter.AddToNodeIpset(nodeIP)
+}
+
+// RemoveFromNodeIpset wraps the corresponding iptables.Manager method.
+func (nm *nodeManager) RemoveFromNodeIpset(nodeIP net.IP) {
+	nm.ipsetter.RemoveFromNodeIpset(nodeIP)
 }
 
 // needsEncapsulation returns whether tunnel encapsulation shall be used towards
@@ -116,6 +151,72 @@ func (nm *nodeManager) routingMode(node *nodeTypes.Node, verbose bool) routingMo
 	}
 
 	return mode
+}
+
+// updateIpsetSkipSet updates the set of IPs for which the ipset entry insertion
+// and removal should be skipped. Specifically, it includes all NodeInternalIP
+// addresses of nodes towards which tunnel routing is preferred.
+func (nm *nodeManager) updateIpsetSkipSet(old, new *nodeTypes.Node, log logrus.FieldLogger) {
+	// Don't add the IPs to the exclusion list if native routing is preferred.
+	if new != nil && !nm.needsEncapsulation(new) {
+		new = nil
+	}
+
+	onAdded := func(ip net.IP) {
+		ipstr := ip.String()
+		if _, loaded := nm.ipsetSkip.LoadOrStore(ipstr, struct{}{}); !loaded {
+			log.WithField(logfields.IPAddr, ipstr).Debug("Added IP address to ipset exclusion list")
+		}
+	}
+
+	onDeleted := func(ip net.IP) {
+		ipstr := ip.String()
+		if _, loaded := nm.ipsetSkip.LoadAndDelete(ipstr); loaded {
+			log.WithField(logfields.IPAddr, ipstr).Debug("Removed IP address from ipset exclusion list")
+		}
+	}
+
+	nm.forEachAddress(old, new, false /* InternalIPs only */, onAdded, onDeleted)
+}
+
+// forEachAddress respectively executes the onAdded and onDeleted functions for
+// each NodeInternalIP (and NodeExternalIP if includeExternalIPs is set) that is
+// different between the old and the new node.
+func (nm *nodeManager) forEachAddress(old, new *nodeTypes.Node, includeExternalIPs bool, onAdded, onDeleted func(net.IP)) {
+	var (
+		oldIPs []net.IP
+		newIPs []net.IP
+		filter = func(ip net.IP) bool { return ip == nil || ip.IsUnspecified() }
+	)
+
+	if old != nil {
+		oldIPs = append(oldIPs, old.GetNodeInternalIPv4(), old.GetNodeInternalIPv6())
+		if includeExternalIPs {
+			oldIPs = append(oldIPs, old.GetExternalIP(false /* IPv4 */), old.GetExternalIP(true /* IPv6 */))
+		}
+	}
+
+	if new != nil {
+		newIPs = append(newIPs, new.GetNodeInternalIPv4(), new.GetNodeInternalIPv6())
+		if includeExternalIPs {
+			newIPs = append(newIPs, new.GetExternalIP(false /* IPv4 */), new.GetExternalIP(true /* IPv6 */))
+		}
+
+		// Drop the addresses that did not change.
+		for i := range oldIPs {
+			if oldIPs[i].Equal(newIPs[i]) {
+				oldIPs[i], newIPs[i] = nil, nil
+			}
+		}
+	}
+
+	for _, ip := range slices.DeleteFunc(oldIPs, filter) {
+		onDeleted(ip)
+	}
+
+	for _, ip := range slices.DeleteFunc(newIPs, filter) {
+		onAdded(ip)
+	}
 }
 
 // nodeManagerLight aliases nodeManager to provide lightweight wrappers of the

pkg/node/manager/enterprise_manager.go
@@ -1,0 +1,27 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+package manager
+
+// CEIPSetManager aliases the private ipsetManager interface.
+type CEIPSetManager = ipsetManager
+
+// InjectCEIPSetManager allows to override the default ipsetManager injected
+// through hive, to support additional enterprise features (e.g., filter ipset
+// entries for mixed routing mode). This method is intended to be executed
+// through an Invoke function before starting the NodeManager subsystem.
+func InjectCEIPSetManager(mgr NodeManager, ipsetter CEIPSetManager) {
+	impl, ok := mgr.(*manager)
+	if !ok {
+		return
+	}
+
+	impl.ipsetMgr = ipsetter
+}

commit feb33adde2daafdcdf062b213fba0642660fa795

mixed routing: rework ipset entries filtering based on routing mode
Normally, Cilium inserts an ipset entry for each NodeInternalIP address
when configured in native routing mode if iptables masquerading is
enabled, to skip masquerading for pod to node traffic. Differently,
masquerading must be performed when running in tunnel mode, otherwise
packets may be dropped, and return traffic would not work.

This commit reworks the previous implementation, removing the wrapper
around the IPSetManager used to filter out ipset upsertion operations
based on the exclusion list configured when new node upsertions and
deletions are observed. Differently, we simply inject a filter function,
which takes a node object as parameter, into Hive, leveraged by the
OSS logic to directly filter out the upsertion operation. That way,
we can directly check the routing mode annotation, avoiding additional
indirections which are potentially fragile (and likely to cause build
conflicts upon OSS changes). We couldn't adopt this approach earlier
because the previous IPSetManager implementation didn't remove possible
stale entries, which could have caused issues if the routing mode was
changed when the Cilium agent was down.

Similarly to before, this logic works under the assumption that the
fallback routing mode is tunnel (i.e., the only supported configuration
at the moment). Hence, it filters out the ipset entries associated
with nodes towards which the preferred routing mode is tunnel.
To support native routing fallback, instead, we should also enable
the ipset manager logic in that case (as otherwise disabled).

Fixes: ca0ef85546b4 ("mixed routing: filter ipset entries creation based on routing mode")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

enterprise/pkg/mixedrouting/cell.go
@@ -30,6 +30,10 @@ var Cell = cell.Module(
 		// and the compilation of the corresponding logic when the primary routing
 		// mode is native, and fallback it tunnel.
 		datapathConfigProvider,
+
+		// Configure the filter to limit the upsertion of ipset entries to only the
+		// ones corresponding to nodes that should be reached in native routing mode.
+		(*manager).ipsetFilter,
 	),
 
 	cell.Invoke(
@@ -41,7 +45,7 @@ var Cell = cell.Module(
 
 		// Hook the extra logic to observe node upsertions and deletions, retrieve
 		// the routing modes annotations and configure the datapath accordingly
-		// (e.g., node routes, ipset entries, ...).
+		// (e.g., node routes, ...).
 		(*manager).setupNodeManager,
 
 		// Hook the extra logic to observe endpoint upsertions and deletions,

enterprise/pkg/mixedrouting/mixedrouting.go
@@ -22,7 +22,6 @@ import (
 
 	"github.com/cilium/cilium/pkg/clustermesh"
 	dpipc "github.com/cilium/cilium/pkg/datapath/ipcache"
-	"github.com/cilium/cilium/pkg/datapath/iptables"
 	linuxdatapath "github.com/cilium/cilium/pkg/datapath/linux"
 	"github.com/cilium/cilium/pkg/datapath/tunnel"
 	datapath "github.com/cilium/cilium/pkg/datapath/types"
@@ -92,9 +91,7 @@ type params struct {
 	DaemonConfig *option.DaemonConfig
 	Tunnel       tunnel.Config
 
-	NodeManager     nodemanager.NodeManager
-	IPTablesManager *iptables.Manager
-	IPCache         *ipcache.IPCache
+	IPCache *ipcache.IPCache
 
 	Metrics Metrics
 }
@@ -123,11 +120,9 @@ func newManager(in params) *manager {
 	// The node manager must be used in its lightweight form when
 	// mgr.enabledWithFallback() is false, as is only partially initialized.
 	mgr.nodes = &nodeManager{
-		logger:     mgr.logger,
-		modes:      mgr.modes,
-		downstream: in.NodeManager,
-		ipsetter:   in.IPTablesManager,
-		epmapper:   mgr.endpoints,
+		logger:   mgr.logger,
+		modes:    mgr.modes,
+		epmapper: mgr.endpoints,
 	}
 
 	return &mgr
@@ -144,6 +139,9 @@ func (mgr *manager) configureLocalNode(lns *node.LocalNodeStore) {
 }
 
 func (mgr *manager) setupNodeManager(dp datapath.Datapath, cm *clustermesh.ClusterMesh, nomgr nodemanager.NodeManager) {
+	// The downstream is configured here to avoid a circular dependency in Hive.
+	mgr.nodes.downstream = nomgr
+
 	// We don't need to hook the extra logic if the local node is configured
 	// with a single routing mode, as it will be always selected anyway.
 	// However, we still inject the lightweight version of the node manager
@@ -155,15 +153,21 @@ func (mgr *manager) setupNodeManager(dp datapath.Datapath, cm *clustermesh.Clust
 
 	clustermesh.InjectCENodeObserver(cm, mgr.nodes)
 	linuxdatapath.InjectCEEnableEncapsulation(dp, mgr.nodes.needsEncapsulation)
+}
+
+func (mgr *manager) ipsetFilter() nodemanager.IPSetFilterFn {
+	if !mgr.enabledWithFallback() {
+		return nil
+	}
 
 	// Note: the current approach works assuming that the fallback routing mode
 	// is tunnel (the only supported configuration at the moment), by filtering
 	// out the ipset insertions/deletions if the preferred routing mode towards
 	// the given node is tunneling (and the local routing mode is set to native,
 	// otherwise they would not have been inserted in the first place). To support
-	// native routing fallback, instead, we should force the creation of the ipset
-	// and manually insert the appropriate entries.
-	nodemanager.InjectCEIPSetManager(nomgr, mgr.nodes)
+	// native routing fallback, instead, we should also enable the ipset manager
+	// logic in that case (as otherwise disabled).
+	return mgr.nodes.ipsetFilter
 }
 
 func (mgr *manager) setupEndpointManager(cm *clustermesh.ClusterMesh, lst *dpipc.BPFListener) {

enterprise/pkg/mixedrouting/node.go
@@ -18,7 +18,6 @@ import (
 
 	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
-	nodemanager "github.com/cilium/cilium/pkg/node/manager"
 	"github.com/cilium/cilium/pkg/node/store"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 )
@@ -34,9 +33,6 @@ type nodeManager struct {
 
 	downstream store.NodeManager
 	nodesCache lock.Map[nodeTypes.Identity, *nodeTypes.Node]
-
-	ipsetter  nodemanager.CEIPSetManager
-	ipsetSkip lock.Map[string, struct{}]
 
 	epmapper endpointMapper
 }
@@ -69,7 +65,6 @@ func (nm *nodeManager) NodeUpdated(node nodeTypes.Node) {
 	log.Debug("Observed node upsertion")
 	nm.nodesCache.Store(id, &node)
 
-	nm.updateIpsetSkipSet(prev, &node, log)
 	nm.updateEndpointAssociations(prev, &node, log)
 
 	nm.downstream.NodeUpdated(node)
@@ -90,37 +85,21 @@ func (nm *nodeManager) NodeDeleted(node nodeTypes.Node) {
 	log.Debug("Observed node deletion")
 	nm.nodesCache.Delete(id)
 
-	nm.updateIpsetSkipSet(&node, nil, log)
 	nm.updateEndpointAssociations(&node, nil, log)
 
 	nm.downstream.NodeDeleted(node)
 }
 
-// AddToNodeIpset wraps the corresponding iptables.Manager method, skipping the
-// insertion if the IP address belongs to the exclusion list (i.e., it was
-// configured to prefer tunnel routing).
-func (nm *nodeManager) AddToNodeIpset(nodeIP net.IP) {
-	ipstr := nodeIP.String()
-	if _, ok := nm.ipsetSkip.Load(ipstr); ok {
-		nm.logger.WithField(logfields.IPAddr, ipstr).
-			Debug("Skipping ipset insertion, as the IP belongs to the exclusion list")
-
-		// Ensure that no stale entry is present.
-		nm.RemoveFromNodeIpset(nodeIP)
-		return
-	}
-
-	nm.ipsetter.AddToNodeIpset(nodeIP)
-}
-
-// RemoveFromNodeIpset wraps the corresponding iptables.Manager method.
-func (nm *nodeManager) RemoveFromNodeIpset(nodeIP net.IP) {
-	nm.ipsetter.RemoveFromNodeIpset(nodeIP)
-}
-
 // needsEncapsulation returns whether tunnel encapsulation shall be used towards
 // the given remote node. It fallbacks to the local primary mode when unknown.
 func (nm *nodeManager) needsEncapsulation(node *nodeTypes.Node) bool {
+	return needsEncapsulation(nm.routingMode(node, false /* silent */))
+}
+
+// ipsetFilter returns whether the insertion of the ipset entries for the given
+// node should be filtered out (i.e., when the preferred routing mode towards
+// the given node is tunneling).
+func (nm *nodeManager) ipsetFilter(node *nodeTypes.Node) bool {
 	return needsEncapsulation(nm.routingMode(node, false /* silent */))
 }
 
@@ -160,32 +139,6 @@ func (nm *nodeManager) routingMode(node *nodeTypes.Node, verbose bool) routingMo
 	}
 
 	return mode
-}
-
-// updateIpsetSkipSet updates the set of IPs for which the ipset entry insertion
-// and removal should be skipped. Specifically, it includes all NodeInternalIP
-// addresses of nodes towards which tunnel routing is preferred.
-func (nm *nodeManager) updateIpsetSkipSet(old, new *nodeTypes.Node, log logrus.FieldLogger) {
-	// Don't add the IPs to the exclusion list if native routing is preferred.
-	if new != nil && !nm.needsEncapsulation(new) {
-		new = nil
-	}
-
-	onAdded := func(ip net.IP) {
-		ipstr := ip.String()
-		if _, loaded := nm.ipsetSkip.LoadOrStore(ipstr, struct{}{}); !loaded {
-			log.WithField(logfields.IPAddr, ipstr).Debug("Added IP address to ipset exclusion list")
-		}
-	}
-
-	onDeleted := func(ip net.IP) {
-		ipstr := ip.String()
-		if _, loaded := nm.ipsetSkip.LoadAndDelete(ipstr); loaded {
-			log.WithField(logfields.IPAddr, ipstr).Debug("Removed IP address from ipset exclusion list")
-		}
-	}
-
-	nm.forEachAddress(old, new, false /* InternalIPs only */, onAdded, onDeleted)
 }
 
 // updateEndpointAssociations updates the mappings between Node{Internal,External}IP

pkg/node/manager/enterprise_manager.go
@@ -1,27 +1,0 @@
-//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
-//
-//  NOTICE: All information contained herein is, and remains the property of
-//  Isovalent Inc and its suppliers, if any. The intellectual and technical
-//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
-//  and may be covered by U.S. and Foreign Patents, patents in process, and are
-//  protected by trade secret or copyright law.  Dissemination of this information
-//  or reproduction of this material is strictly forbidden unless prior written
-//  permission is obtained from Isovalent Inc.
-
-package manager
-
-// CEIPSetManager aliases the private ipsetManager interface.
-type CEIPSetManager = ipsetManager
-
-// InjectCEIPSetManager allows to override the default ipsetManager injected
-// through hive, to support additional enterprise features (e.g., filter ipset
-// entries for mixed routing mode). This method is intended to be executed
-// through an Invoke function before starting the NodeManager subsystem.
-func InjectCEIPSetManager(mgr NodeManager, ipsetter CEIPSetManager) {
-	impl, ok := mgr.(*manager)
-	if !ok {
-		return
-	}
-
-	impl.ipsetMgr = ipsetter
-}

enterprise/pkg/mixedrouting/node_test.go
@@ -16,13 +16,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"golang.org/x/exp/maps"
 
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/node/addressing"
-	nodemanager "github.com/cilium/cilium/pkg/node/manager"
 	nodeTypes "github.com/cilium/cilium/pkg/node/types"
-	"github.com/cilium/cilium/pkg/slices"
 )
 
 type op string
@@ -38,51 +35,22 @@ type fakeNodeEntry struct {
 }
 
 type fakeNodeDownstream struct {
-	ops      []fakeNodeEntry
-	ipsetter nodemanager.CEIPSetManager
-	nodeIPs  []string
+	ops []fakeNodeEntry
 }
 
-func newFakeNodeDownstream(ipsetter nodemanager.CEIPSetManager) *fakeNodeDownstream {
-	return &fakeNodeDownstream{ipsetter: ipsetter}
+func newFakeNodeDownstream() *fakeNodeDownstream {
+	return &fakeNodeDownstream{}
 }
 
 func (fd *fakeNodeDownstream) clear() { fd.ops = nil }
 
 func (fd *fakeNodeDownstream) NodeUpdated(node nodeTypes.Node) {
 	fd.ops = append(fd.ops, fakeNodeEntry{opUpsert, node})
-
-	// Simulate the behavior of the real NodeUpdated implementation.
-	var ipscache []string
-	for _, addr := range node.IPAddresses {
-		if addr.Type == addressing.NodeInternalIP {
-			ipscache = append(ipscache, addr.IP.String())
-			fd.ipsetter.AddToNodeIpset(addr.IP)
-		}
-	}
-
-	for _, addr := range slices.Diff(fd.nodeIPs, ipscache) {
-		fd.ipsetter.RemoveFromNodeIpset(net.ParseIP(addr))
-	}
-
-	fd.nodeIPs = ipscache
 }
 
 func (fd *fakeNodeDownstream) NodeDeleted(node nodeTypes.Node) {
 	fd.ops = append(fd.ops, fakeNodeEntry{opDelete, node})
-	for _, addr := range node.IPAddresses {
-		if addr.Type == addressing.NodeInternalIP {
-			fd.ipsetter.RemoveFromNodeIpset(addr.IP)
-		}
-	}
-	fd.nodeIPs = nil
 }
-
-type fakeIPSetter map[string]struct{}
-
-func newFakeIPSetter() fakeIPSetter                    { return make(map[string]struct{}) }
-func (fis fakeIPSetter) AddToNodeIpset(ip net.IP)      { fis[ip.String()] = struct{}{} }
-func (fis fakeIPSetter) RemoveFromNodeIpset(ip net.IP) { delete(fis, ip.String()) }
 
 type fakeEPMapper map[string]routingModeType
 
@@ -118,14 +86,13 @@ func newNode(name string, id uint32, modes routingModesType, internalIPs []strin
 }
 
 func TestNodeManager(t *testing.T) {
+	fd := newFakeNodeDownstream()
 	mgr := nodeManager{
-		logger:   logging.DefaultLogger,
-		modes:    routingModesType{routingModeNative, routingModeVXLAN},
-		epmapper: newFakeEPMapper(),
-		ipsetter: newFakeIPSetter(),
+		logger:     logging.DefaultLogger,
+		modes:      routingModesType{routingModeNative, routingModeVXLAN},
+		downstream: fd,
+		epmapper:   newFakeEPMapper(),
 	}
-	fd := newFakeNodeDownstream(&mgr)
-	mgr.downstream = fd
 
 	ips1 := []string{"10.1.2.3", "fd00::1234"}
 	ips2 := []string{"10.1.2.3", "fd00::6789"}
@@ -135,20 +102,15 @@ func TestNodeManager(t *testing.T) {
 	no3 := *newNode("foo", 3, []routingModeType{routingModeNative}, ips2)
 	no4 := *newNode("foo", 4, []routingModeType{routingModeNative}, ips1)
 
-	// Simulate the presence of a stale ipset entry
-	mgr.ipsetter.AddToNodeIpset(net.ParseIP("10.1.2.3"))
-
 	mgr.NodeUpdated(no1)
 	require.Len(t, fd.ops, 1, "Insertion should propagate to downstream")
 	require.Equal(t, fakeNodeEntry{opUpsert, no1}, fd.ops[0], "Insertion should propagate to downstream")
-	require.Empty(t, mgr.ipsetter.(fakeIPSetter), "ipset not configured correctly")
 	require.Equal(t, toMapping(ips1, routingModeVXLAN), mgr.epmapper.(fakeEPMapper), "endpoint mapping not configured correctly")
 	fd.clear()
 
 	mgr.NodeUpdated(no2)
 	require.Len(t, fd.ops, 1, "Update should propagate to downstream")
 	require.Equal(t, fakeNodeEntry{opUpsert, no2}, fd.ops[0], "Update should propagate to downstream")
-	require.Empty(t, mgr.ipsetter.(fakeIPSetter), "ipset not configured correctly")
 	require.Equal(t, toMapping(ips2, routingModeVXLAN), mgr.epmapper.(fakeEPMapper), "endpoint mapping not configured correctly")
 	fd.clear()
 
@@ -156,14 +118,12 @@ func TestNodeManager(t *testing.T) {
 	require.Len(t, fd.ops, 2, "Routing mode change should trigger deletion followed by insertion")
 	require.Equal(t, fakeNodeEntry{opDelete, no2}, fd.ops[0], "Routing mode change should trigger deletion followed by insertion")
 	require.Equal(t, fakeNodeEntry{opUpsert, no3}, fd.ops[1], "Routing mode change should trigger deletion followed by insertion")
-	require.ElementsMatch(t, maps.Keys(mgr.ipsetter.(fakeIPSetter)), ips2, "ipset not configured correctly")
 	require.Equal(t, toMapping(ips2, routingModeNative), mgr.epmapper.(fakeEPMapper), "endpoint mapping not configured correctly")
 	fd.clear()
 
 	mgr.NodeUpdated(no4)
 	require.Len(t, fd.ops, 1, "Update should propagate to downstream")
 	require.Equal(t, fakeNodeEntry{opUpsert, no4}, fd.ops[0], "Update should propagate to downstream")
-	require.ElementsMatch(t, maps.Keys(mgr.ipsetter.(fakeIPSetter)), ips1, "ipset not configured correctly")
 	require.Equal(t, toMapping(ips1, routingModeNative), mgr.epmapper.(fakeEPMapper), "endpoint mapping not configured correctly")
 	fd.clear()
 
@@ -171,14 +131,12 @@ func TestNodeManager(t *testing.T) {
 	require.Len(t, fd.ops, 2, "Routing mode change should trigger deletion followed by insertion")
 	require.Equal(t, fakeNodeEntry{opDelete, no4}, fd.ops[0], "Routing mode change should trigger deletion followed by insertion")
 	require.Equal(t, fakeNodeEntry{opUpsert, no2}, fd.ops[1], "Routing mode change should trigger deletion followed by insertion")
-	require.Empty(t, mgr.ipsetter.(fakeIPSetter), "ipset not configured correctly")
 	require.Equal(t, toMapping(ips2, routingModeVXLAN), mgr.epmapper.(fakeEPMapper), "endpoint mapping not configured correctly")
 	fd.clear()
 
 	mgr.NodeDeleted(no2)
 	require.Len(t, fd.ops, 1, "Deletion should propagate to downstream")
 	require.Equal(t, fakeNodeEntry{opDelete, no2}, fd.ops[0], "Deletion should propagate to downstream")
-	require.Empty(t, mgr.ipsetter.(fakeIPSetter), "ipset not configured correctly")
 	require.Empty(t, mgr.epmapper.(fakeEPMapper), "endpoint mapping not reset correctly")
 	fd.clear()
 }
@@ -227,7 +185,9 @@ func TestNodeManagerNeedsEncapsulation(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("%s|%s", tt.local, tt.remote), func(t *testing.T) {
 			mgr := nodeManager{logger: logging.DefaultLogger, modes: tt.local}
-			require.Equal(t, tt.expected, mgr.needsEncapsulation(newNode("foo", 0, tt.remote, nil)))
+			node := newNode("foo", 0, tt.remote, nil)
+			require.Equal(t, tt.expected, mgr.needsEncapsulation(node))
+			require.Equal(t, tt.expected, mgr.ipsetFilter(node))
 		})
 	}
 }