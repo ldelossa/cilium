018f9a9acd11585d30326efc6e5d56eb93d8a2c4
Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
2024-06-11 12:04:23 +0900

    bgpv2: Introduce OSS -> CEE reconcile parameter upgrader
    In BGPv2 for CEE, we don't watch the IsovalentBGPXXX resources directly.
    Instead, the operator creates the mirror OSS resource and the events for
    the OSS resources triggers the reconciliation.
    
    Also, in the agent, we implement enterprise-specific reconcilers as a
    ConfigReconciler which takes OSS resource as an argument. Therefore, in
    the enterprise-specific reconcilers, we need to resolve CEE resources
    from OSS resources to get an actual configuration.
    
    We introduce reconcilerParamsUpgrader object which implements a method
    upgrade. It takes OSS ReconcileParams and "upgrades" it to
    EnterpriseReconcileParams by fetching IsovalentBGPNodeConfig and find
    the BGPInstance corresponds to the OSS instance we're currently
    reconciling.
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
    Signed-off-by: harsimran pabla <hpabla@isovalent.com>

---
+++ enterprise/pkg/bgpv1/manager/reconcilerv2/upgrader.go
@@ -0,0 +1,111 @@
+// Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of
+// Isovalent Inc and its suppliers, if any. The intellectual and technical
+// concepts contained herein are proprietary to Isovalent Inc and its suppliers
+// and may be covered by U.S. and Foreign Patents, patents in process, and are
+// protected by trade secret or copyright law.  Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written
+// permission is obtained from Isovalent Inc.
+
+package reconcilerv2
+
+import (
+	"context"
+	"fmt"
+	"sync/atomic"
+
+	"github.com/cilium/hive/cell"
+	"github.com/cilium/hive/job"
+
+	"github.com/cilium/cilium/pkg/bgpv1/manager/reconcilerv2"
+	"github.com/cilium/cilium/pkg/bgpv1/types"
+	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/k8s/apis/isovalent.com/v1alpha1"
+	"github.com/cilium/cilium/pkg/k8s/resource"
+)
+
+// EnterpriseReconcileParams is an enterprise specific version of
+// reconcilerv2.ReconcileParams. It must be created with
+// reconcileParamsUpgrader.upgrade.
+type EnterpriseReconcileParams struct {
+	BGPInstance   *EnterpriseBGPInstance
+	DesiredConfig *v1alpha1.IsovalentBGPNodeInstance
+	CiliumNode    *ciliumv2.CiliumNode
+}
+
+// EnterpriseBGPInstance is an enterprise specific version of
+// reconcilerv2.BGPInstance. It must be created with
+// reconcileParamsUpgrader.upgrade.
+type EnterpriseBGPInstance struct {
+	Config *v1alpha1.IsovalentBGPNodeInstance
+	Router types.Router
+
+	// Shallow copy of the metadata from the reconcilerv2.BGPInstance. You
+	// can put enterprise-specific metadata directly into this map and it
+	// will be persisted between reconciliations. Thus, you are
+	// responsible for ensuring there's no key collision between the OSS
+	// reconcilers.
+	Metadata map[string]any
+}
+
+type reconcileParamsUpgrader struct {
+	initialized atomic.Bool
+	store       resource.Store[*v1alpha1.IsovalentBGPNodeConfig]
+}
+
+func newReconcileParamsUpgrader(r resource.Resource[*v1alpha1.IsovalentBGPNodeConfig], g job.Group) *reconcileParamsUpgrader {
+	u := &reconcileParamsUpgrader{}
+
+	g.Add(job.OneShot("bgp-reconcile-params-upgrader-init", func(ctx context.Context, health cell.Health) error {
+		s, err := r.Store(ctx)
+		if err != nil {
+			return err
+		}
+		u.store = s
+		u.initialized.Store(true)
+		return nil
+	}))
+
+	return u
+}
+
+func (u *reconcileParamsUpgrader) upgrade(params reconcilerv2.ReconcileParams) (EnterpriseReconcileParams, error) {
+	if !u.initialized.Load() {
+		return EnterpriseReconcileParams{}, fmt.Errorf("not initialized")
+	}
+
+	if params.BGPInstance == nil || params.DesiredConfig == nil || params.CiliumNode == nil {
+		return EnterpriseReconcileParams{}, fmt.Errorf("invalid params")
+	}
+
+	nc, exists, err := u.store.GetByKey(resource.Key{Name: params.CiliumNode.Name})
+	if err != nil {
+		return EnterpriseReconcileParams{}, err
+	}
+
+	if !exists {
+		return EnterpriseReconcileParams{}, fmt.Errorf("enterprise node config not found")
+	}
+
+	for i, inst := range nc.Spec.BGPInstances {
+		if inst.Name != params.BGPInstance.Config.Name {
+			continue
+		}
+		return EnterpriseReconcileParams{
+			BGPInstance: &EnterpriseBGPInstance{
+				// So far, we don't need to keep the previous
+				// config. Once we have a use case for it, we
+				// can consider storing it in the metadata and
+				// copying it here.
+				Config:   nil,
+				Router:   params.BGPInstance.Router,
+				Metadata: params.BGPInstance.Metadata,
+			},
+			DesiredConfig: &nc.Spec.BGPInstances[i],
+			CiliumNode:    params.CiliumNode,
+		}, nil
+	}
+
+	return EnterpriseReconcileParams{}, fmt.Errorf("enterprise node instance not found")
+}
---
+++ enterprise/pkg/bgpv1/manager/reconcilerv2/upgrader_test.go
@@ -0,0 +1,160 @@
+// Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of
+// Isovalent Inc and its suppliers, if any. The intellectual and technical
+// concepts contained herein are proprietary to Isovalent Inc and its suppliers
+// and may be covered by U.S. and Foreign Patents, patents in process, and are
+// protected by trade secret or copyright law.  Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written
+// permission is obtained from Isovalent Inc.
+
+package reconcilerv2
+
+import (
+	"context"
+	"log/slog"
+	"testing"
+	"time"
+
+	"github.com/cilium/hive"
+	"github.com/cilium/hive/cell"
+	"github.com/cilium/hive/job"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/cilium/cilium/enterprise/operator/pkg/bgpv2/config"
+	"github.com/cilium/cilium/pkg/bgpv1/manager/instance"
+	"github.com/cilium/cilium/pkg/bgpv1/manager/reconcilerv2"
+	"github.com/cilium/cilium/pkg/bgpv1/types"
+	"github.com/cilium/cilium/pkg/k8s"
+	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
+	"github.com/cilium/cilium/pkg/k8s/apis/isovalent.com/v1alpha1"
+	"github.com/cilium/cilium/pkg/k8s/client"
+)
+
+func TestReconcileParamsUpgrader(t *testing.T) {
+	var (
+		up *reconcileParamsUpgrader
+		cs client.Clientset
+		jg job.Group
+	)
+
+	h := hive.New(
+		job.Cell,
+		cell.Provide(
+			newReconcileParamsUpgrader,
+			client.NewFakeClientset,
+			k8s.CiliumBGPNodeConfigResource,
+			k8s.IsovalentBGPNodeConfigResource,
+			cell.NewSimpleHealth,
+			func(r job.Registry, health cell.Health) job.Group {
+				return r.NewGroup(health)
+			},
+			// enterprise bgp is enabled
+			func() config.Config {
+				return config.Config{
+					Enabled: true,
+				}
+			},
+		),
+		cell.Invoke(func(u *reconcileParamsUpgrader, c client.Clientset, j job.Group) {
+			up = u
+			cs = c
+			jg = j
+		}),
+	)
+
+	err := h.Start(slog.Default(), context.Background())
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		h.Stop(slog.Default(), context.Background())
+	})
+
+	// start jobs in the group
+	jg.Start(context.Background())
+
+	ceeNode, err := cs.IsovalentV1alpha1().IsovalentBGPNodeConfigs().Create(
+		context.Background(),
+		&v1alpha1.IsovalentBGPNodeConfig{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "node0",
+			},
+			Spec: v1alpha1.IsovalentBGPNodeSpec{
+				BGPInstances: []v1alpha1.IsovalentBGPNodeInstance{
+					{
+						Name: "instance0",
+					},
+				},
+			},
+		},
+		metav1.CreateOptions{},
+	)
+	require.NoError(t, err)
+
+	ossNode, err := cs.CiliumV2alpha1().CiliumBGPNodeConfigs().Create(
+		context.Background(),
+		&v2alpha1.CiliumBGPNodeConfig{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "node0",
+			},
+			Spec: v2alpha1.CiliumBGPNodeSpec{
+				BGPInstances: []v2alpha1.CiliumBGPNodeInstance{
+					{
+						Name: ceeNode.Spec.BGPInstances[0].Name,
+					},
+				},
+			},
+		},
+		metav1.CreateOptions{},
+	)
+	require.NoError(t, err)
+
+	ossParams := reconcilerv2.ReconcileParams{
+		BGPInstance: &instance.BGPInstance{
+			Config: &ossNode.Spec.BGPInstances[0],
+			Router: types.NewFakeRouter(),
+			Metadata: map[string]any{
+				"foo": "bar",
+			},
+		},
+		DesiredConfig: &ossNode.Spec.BGPInstances[0],
+		CiliumNode: &ciliumv2.CiliumNode{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "node0",
+			},
+		},
+	}
+
+	var ceeParams EnterpriseReconcileParams
+	require.EventuallyWithT(t, func(ct *assert.CollectT) {
+		ceeParams, err = up.upgrade(ossParams)
+		if !assert.NoError(ct, err) {
+			return
+		}
+	}, time.Second*3, time.Millisecond*100)
+
+	require.True(t,
+		// Pointer equality
+		ceeParams.BGPInstance.Router == ossParams.BGPInstance.Router,
+		"CEE router doesn't point to the same router instance as OSS",
+	)
+
+	// Ensure the change to the metadata in CEE is visible in OSS
+	ceeParams.BGPInstance.Metadata["baz"] = "qux"
+	require.Equal(t,
+		ceeParams.BGPInstance.Metadata["foo"], ossParams.BGPInstance.Metadata["foo"],
+		"CEE Metadata must be a shallow copy of OSS Metadata (mismatched value for \"foo\")",
+	)
+	require.Equal(t,
+		ceeParams.BGPInstance.Metadata["baz"], ossParams.BGPInstance.Metadata["baz"],
+		"CEE Metadata must be a shallow copy of OSS Metadata (mismatched value for \"baz\")",
+	)
+
+	require.True(t,
+		// Pointer equality
+		ceeParams.CiliumNode == ossParams.CiliumNode,
+		"CEE CiliumNode doesn't point to the same router instance as OSS",
+	)
+}
