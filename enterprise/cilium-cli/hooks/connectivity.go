96e7a41b6f72de43280db34faaa60bbaf0a4046a
Rastislav Szabo <rastislav.szabo@isovalent.com>
2024-09-04 15:30:48 +0200

    cilium-cli: Do not run OSS BGPv2 tests if enterprise BGP CP is enabled
    As OSS BGPv2 CRDs can not be used if enterprise BGP CP is enabled,
    do not run OSS BGPv2 tests if enterprise BGP CP is enabled.
    
    Signed-off-by: Rastislav Szabo <rastislav.szabo@isovalent.com>

--- enterprise/cilium-cli/hooks/connectivity.go
+++ enterprise/cilium-cli/hooks/connectivity.go
@@ -352,6 +352,13 @@ func (ec *EnterpriseConnectivity) addMulticastTests(ct *check.ConnectivityTest) 
 }
 
 func (ec *EnterpriseConnectivity) addBGPTests(ct *check.ConnectivityTest) (err error) {
+	// disable OSS BGPv2 tests if enterprise BGP CP is enabled (OSS CRDs can not be used if enterprise CP is enabled)
+	if ossv2Test, err := ct.GetTest("bgp-control-plane-v2"); err == nil {
+		ossv2Test.WithFeatureRequirements(
+			features.RequireDisabled(enterpriseFeatures.EnterpriseBGPControlPlane),
+		)
+	}
+
 	newTest := func(ct *check.ConnectivityTest, name string) *enterpriseCheck.EnterpriseTest {
 		return enterpriseCheck.NewEnterpriseConnectivityTest(ct).
 			NewEnterpriseTest(name).
